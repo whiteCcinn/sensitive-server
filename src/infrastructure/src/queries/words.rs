c3ba07d1675e312e881b4c1092e42c07128073c8
whiteCcinn <471113744@qq.com>
2020-12-14 11:41:44 +0800

    update sensitive_server_infrastructure unit test

--- src/infrastructure/src/queries/words.rs
+++ src/infrastructure/src/queries/words.rs
@@ -4,10 +4,13 @@ use diesel::prelude::*;
 use crate::Repo;
 use diesel::result::Error;
 
-pub fn insert(repo: &Repo, word: NewSensitiveWords) -> Result<SensitiveWords, Error> {
-    diesel::insert_into(sensitive_words::table)
-        .values(&word)
-        .get_result(&repo.conn())
+// pub fn insert(repo: &Repo, word: NewSensitiveWords) -> Result<SensitiveWords, Error> {
+pub fn insert(repo: &Repo, word: NewSensitiveWords) -> QueryResult<usize> {
+    // diesel::insert_into(sensitive_words::table)
+    //     .values(&word)
+    //     .get_result(&repo.conn())
+
+    diesel::insert_into(sensitive_words::table).default_values().execute(&repo.conn())
 }
 
 pub fn delete(repo: &Repo, pid: i32) -> Result<(), Error> {
